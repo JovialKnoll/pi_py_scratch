d0337aa1e97bb2165cc9324fa506873e8cfca40e
JovialKnoll <BrettCKaplan@gmail.com>
2017-06-04 17:58:09 -0400

    playing with a shift register

---
+++ shift.py
@@ -0,0 +1,87 @@
+import sys, time
+import RPi.GPIO as gpio
+
+gpio.setmode(gpio.BCM)
+gpio.setwarnings(False)
+
+single_led = 21
+gpio.setup(single_led, gpio.OUT)
+
+def blinkSingle():
+    gpio.output(single_led, gpio.HIGH)
+    time.sleep(1)
+    gpio.output(single_led, gpio.LOW)
+    time.sleep(1)
+#for i in range(3):
+#    blinkSingle()
+
+data = 13
+latch = 19
+clock = 26
+small_time = 0#0.0001
+
+gpio.setup(data, gpio.OUT)
+gpio.output(data, gpio.LOW)
+
+gpio.setup(clock, gpio.OUT)
+gpio.output(clock, gpio.LOW)
+
+gpio.setup(latch, gpio.OUT)
+gpio.output(latch, gpio.LOW)
+
+def getIntArray(in_string):
+    return [int(x) for x in in_string]
+def setArray(in_string):
+    for i in getIntArray(in_string):
+        if i is 0:
+            gpio.output(data, gpio.LOW)
+        else:
+            gpio.output(data, gpio.HIGH)
+        time.sleep(small_time)
+        gpio.output(clock, gpio.HIGH)
+        time.sleep(small_time)
+        gpio.output(clock, gpio.LOW)
+        gpio.output(data, gpio.LOW)#clear
+        gpio.output(latch, gpio.HIGH)
+        time.sleep(small_time)
+        gpio.output(latch, gpio.LOW)
+def setNumber(num):
+    # 0 <= num <= 8
+    s = ''.join(['1' for n in range(num)])
+    s += ''.join(['0' for n in range(8 - num)])
+    setArray(s)
+
+slow_time = 0.5
+quick_time = 0.1
+setArray('00000000')
+time.sleep(slow_time)
+setArray('10000001')
+time.sleep(slow_time)
+setArray('11000011')
+time.sleep(slow_time)
+setArray('11100111')
+time.sleep(slow_time)
+setArray('01111110')
+time.sleep(slow_time)
+setArray('00111100')
+time.sleep(slow_time)
+setArray('00011000')
+time.sleep(slow_time)
+setArray('00100100')
+time.sleep(slow_time)
+setArray('01000010')
+time.sleep(slow_time)
+setArray('10000001')
+time.sleep(slow_time)
+for i in range(9):
+    print i
+    setNumber(i)
+    time.sleep(quick_time)
+for i in range(8):
+    print 7 - i
+    setNumber(8 - i)
+    time.sleep(quick_time)
+
+setArray('00000000')
+gpio.cleanup()
+sys.exit()
