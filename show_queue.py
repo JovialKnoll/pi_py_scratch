4a46079161dc1a87b48f80dd28b967453d3872a7
JovialKnoll <BrettCKaplan@gmail.com>
2018-11-26 18:31:19 -0500

    let's display the current queued print jobs here

---
+++ show_queue.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+
+import RPi.GPIO as gpio
+
+#setup
+gpio.setmode(gpio.BCM)
+gpio.setwarnings(False)
+data = 13
+latch = 19
+clock = 26
+gpio.setup(data, gpio.OUT)
+gpio.output(data, gpio.LOW)
+gpio.setup(clock, gpio.OUT)
+gpio.output(clock, gpio.LOW)
+gpio.setup(latch, gpio.OUT)
+gpio.output(latch, gpio.LOW)
+
+def getIntArray(in_string):
+    return [int(x) for x in in_string]
+
+def getIOArray(in_string):
+    return [gpio.LOW if i is 0 else gpio.HIGH for i in getIntArray(in_string)]
+
+REVERSE = True
+
+def setArray(in_string):
+    right_string = in_string
+    if REVERSE:
+        right_string = reversed(in_string)
+    gpio.output(latch, gpio.LOW)
+    for i in getIOArray(right_string):
+        gpio.output(data, i)
+        gpio.output(clock, gpio.HIGH)
+        gpio.output(clock, gpio.LOW)
+        gpio.output(data, gpio.LOW) #clear
+    gpio.output(latch, gpio.HIGH)
+
+def setNumber(num):
+    # 0 <= num <= 8
+    s = ''.join(['1' for n in range(num)])
+    s += ''.join(['0' for n in range(8 - num)])
+    setArray(s)
+
+command = 'lpstat -o HP_Deskjet_D1520 | wc -l'
+response = subprocess.check_output(command, shell=True)
+queue_length = int(response)
+setNumber(queue_length)
+
+gpio.cleanup()
+sys.exit()
