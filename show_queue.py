7c0f89c1fb7e885817e5f9ab1961942f892df9aa
Brett Kaplan <BrettCKaplan@gmail.com>
2018-11-27 14:53:40 -0500

    reorg

--- show_queue.py
+++ show_queue.py
@@ -2,16 +2,9 @@
 
 import sys
 import subprocess
-
-command = 'lpstat -o HP_Deskjet_D1520 | wc -l'
-response = subprocess.check_output(command, shell=True)
-queue_length = int(response)
-
-if (queue_length == 0):
-    sys.exit()
-
-#setup
 import RPi.GPIO as gpio
+
+# gpio setup
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
 data = 13
@@ -23,11 +16,14 @@ gpio.setup(clock, gpio.OUT)
 gpio.output(clock, gpio.LOW)
 gpio.setup(latch, gpio.OUT)
 gpio.output(latch, gpio.LOW)
+REVERSE = True
+
 def getIntArray(in_string):
     return [int(x) for x in in_string]
+
 def getIOArray(in_string):
     return [gpio.LOW if i is 0 else gpio.HIGH for i in getIntArray(in_string)]
-REVERSE = True
+
 def setArray(in_string):
     right_string = in_string
     if REVERSE:
@@ -39,13 +35,21 @@ def setArray(in_string):
         gpio.output(clock, gpio.LOW)
         gpio.output(data, gpio.LOW) #clear
     gpio.output(latch, gpio.HIGH)
+
 def setNumber(num):
     # 0 <= num <= 8
     s = ''.join(['1' for n in range(min(num, 8))])
     s += ''.join(['0' for n in range(8 - min(num, 8))])
     setArray(s)
 
-setNumber(queue_length)
+queue = 'HP_Deskjet_D1520'
+command = 'lpstat -o {} | wc -l'.format(queue)
+def getQueueLength():
+    response = subprocess.check_output(command, shell=True)
+    return int(response)
+
+# loop or something here
+setNumber(getQueueLength())
 
 gpio.cleanup()
 sys.exit()
