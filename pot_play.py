062e28b3ce3550d123774d746e0703c57ec0d5e6
JovialKnoll <BrettCKaplan@gmail.com>
2019-12-09 20:42:59 -0500

    don't use set, use gpio values

--- pot_play.py
+++ pot_play.py
@@ -7,7 +7,7 @@ import RPi.GPIO as gpio
 # gpio setup
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
-set = 20
+send = 20
 test = 21
 led_r = 25
 led_g = 12
@@ -18,22 +18,24 @@ gpio.setup(led_g, gpio.OUT, initial=gpio.LOW)
 gpio.setup(led_b, gpio.OUT, initial=gpio.LOW)
 
 def discharge():
-    gpio.setup(set, gpio.IN)
+    gpio.setup(send, gpio.IN)
     gpio.setup(test, gpio.OUT)
-    gpio.output(test, False)
+    gpio.output(test, gpio.LOW)
     time.sleep(0.001)
 
 def get_value(dt):
     result = dt * 100000
     result -= 5
+    result = max(result, 0)
+    result = min(result, 34)
     return result
 
 def charge_time():
     gpio.setup(test, gpio.IN)
-    gpio.setup(set, gpio.OUT)
+    gpio.setup(send, gpio.OUT)
     start = time.time()
-    gpio.output(set, True)
-    while not gpio.input(test):
+    gpio.output(send, gpio.HIGH)
+    while gpio.input(test) == gpio.LOW:
         pass
     return get_value(time.time() - start)
 
